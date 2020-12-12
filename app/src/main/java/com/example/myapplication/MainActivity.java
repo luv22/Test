c276541c1175881913fc8e0701c1c3a9f566bf00
Lav Gupta <57129908+luv22@users.noreply.github.com>
2020-12-12 14:27:28 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,17 +25,46 @@ public class MainActivity extends AppCompatActivity {
         TextView  lastName = findViewById(R.id.lastName);
         Spinner spinner = findViewById(R.id.dropdown);
         Button button = findViewById(R.id.capture);
+        HashMap<String,String>map = new HashMap<>();
         ArrayList<String> adapter = new ArrayList<>();
-        adapter.add("Java");
-        adapter.add("Kotline");
-        adapter.add("C#");
+        
+        adapter.add(new Data("1","java").getValue());
+        map.put("java","1");
+        adapter.add(new Data("2","kotlin").getValue());
+        map.put("kotlin","2");
+
         ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,adapter);
         spinner.setAdapter(spinnerAdapter);
-        Intent intent = new Intent(this,MainActivity2.class);
-        intent.putExtra("name",firstName.getText().toString());
-        intent.putExtra("lastname",lastName.getText().toString());
-        intent.putExtra("lang",spinner.getSelectedItem().toString());
-        startActivity(intent);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                intent.putExtra("name",firstName.getText().toString());
+                intent.putExtra("lastname",lastName.getText().toString());
+                intent.putExtra("lang",map.get(spinner.getSelectedItem()));
+                startActivity(intent);
 
+            }
+        });
+
+    }
+
+}
+
+class Data{
+    String key;
+    String value;
+
+    public Data(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
     }
 }
